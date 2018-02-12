3a461d8fa6416f8985ccd8e3116937ba7dff8dd6
vishnu <versatilecommunity@gmail.com>
2018-02-12 13:20:10 +0530

    Add sidebar and Top menu

--- init.rb
+++ init.rb
@@ -7,16 +7,18 @@ Redmine::Plugin.register :redmine_spent_time do
   version '0.0.1'
   url 'https://www.redmine.org/plugins/redmine_spent_time_versatile'
   author_url 'http://versatilecommunity.com/about'  
+  
+  menu :top_menu, :spTime, { :controller => 'sptime', :action => 'index' }, :caption => :label_time_sheet, :if => Proc.new { Object.new.extend(SptimeHelper).checkPermission}
 end
 
-Redmine::MenuManager.map :application_menu do |menu|
-	menu.push :sptime, { :controller => 'sptime', :action => 'index' }, :caption => :label_time
-end
+# Redmine::MenuManager.map :application_menu do |menu|
+	# menu.push :sptime, { :controller => 'sptime', :action => 'index' }, :caption => :label_time
+# end
 
-Redmine::MenuManager.map :project_menu do |menu|
-	menu.push :sptime, {:controller => 'sptime', :action => 'index'}, :param => :project_id, :caption => :label_time,
-              :parent => :new_object
-end
+# Redmine::MenuManager.map :project_menu do |menu|
+	# menu.push :sptime, {:controller => 'sptime', :action => 'index'}, :param => :project_id, :caption => :label_time,
+              # :parent => :new_object
+# end
 
 Redmine::MenuManager.map :admin_menu do |menu|	
 	menu.push :spcompany, { :controller => 'spcompany', :action => 'index' }, :caption => :label_company, :if => Proc.new { Object.new.extend(SptimeHelper).checkCpyPermission}, :html => {:class => 'icon icon-stats'}
