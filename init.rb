baa08c0a0723d301e8d20a040d75b7fb9de23add
vishnu <versatilecommunity@gmail.com>
2018-08-15 14:11:55 +0530

    update version and readme

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_spent_time do
   name 'Redmine Spent Time plugin'
   author 'Versatile Community Inc'
   description 'This is a plugin for Redmine'
-  version '0.0.1'
+  version '0.2'
   url 'https://www.redmine.org/plugins/redmine_spent_time_versatile'
   author_url 'http://versatilecommunity.com/about'  
   
